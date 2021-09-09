bac525678f3eb5660333ecf9342b8d169bca3a9e
hongqiaowei <hongqiaowei@163.com>
2021-09-09 12:13:36 +0800

    First commit

---
+++ src/main/java/we/plugin/demo/DemoPluginFilter.java
@@ -0,0 +1,49 @@
+package we.plugin.demo;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import we.plugin.FizzPluginFilter;
+import we.plugin.FizzPluginFilterChain;
+import we.spring.http.server.reactive.ext.FizzServerHttpRequestDecorator;
+import we.util.NettyDataBufferUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
+
+@Component(DemoPluginFilter.DEMO_PLUGIN) // 必须，且为插件 id
+public class DemoPluginFilter implements FizzPluginFilter {
+
+    public static final String DEMO_PLUGIN = "demoPlugin"; // 插件 id
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, Map<String, Object> config) {
+
+        FizzServerHttpRequestDecorator request = (FizzServerHttpRequestDecorator) exchange.getRequest();
+
+        return
+                request.getBody().defaultIfEmpty(NettyDataBufferUtils.EMPTY_DATA_BUFFER)
+                        .single()
+                        .flatMap(
+                                body -> {
+                                    // String bodyStr = body.toString(StandardCharsets.UTF_8); // 请求体对应的字符串
+                                    // System.err.println("request body: " + bodyStr);
+
+                                    // 若需调整转发到后端接口的请求体，可像下面这样设置新的体和类型
+                                    /*
+                                    String newRequestBody = "client " + bodyStr;
+                                    request.setBody(newRequestBody);
+                                    String newRequestBodyType = MediaType.APPLICATION_ATOM_XML_VALUE;
+                                    request.getHeaders().put(HttpHeaders.CONTENT_TYPE, Collections.singletonList(newRequestBodyType));
+                                    */
+
+                                    System.err.println("this is demo plugin"); // 本插件只输出这个
+
+                                    return FizzPluginFilterChain.next(exchange); // 执行后续插件或其它逻辑
+                                }
+                        );
+    }
+}
